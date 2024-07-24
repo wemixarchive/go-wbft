fd6324040e14a349dbdf7671555ac6f4a4119631
jennwiederholen <jennwiederholen@wemade.com>
2024-07-24 10:17:50 +0900

    Fix: fix lint

--- consensus/qbft/backend/snapshot.go
+++ consensus/qbft/backend/snapshot.go
@@ -19,6 +19,7 @@ package backend
 import (
 	"bytes"
 	"encoding/json"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/qbft"
 	"github.com/ethereum/go-ethereum/consensus/qbft/validator"
