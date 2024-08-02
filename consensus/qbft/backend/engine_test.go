45cae9a9caa633fdae0fc2c8acd13e6d31b19d16
jennwiederholen <jennwiederholen@wemade.com>
2024-08-02 15:36:46 +0900

    Fix: remove test validating mixDigest value

--- consensus/qbft/backend/engine_test.go
+++ consensus/qbft/backend/engine_test.go
@@ -265,14 +265,15 @@ func TestVerifyHeader(t *testing.T) {
 		t.Errorf("error mismatch: have %v, want %v", err, qbftcommon.ErrInvalidExtraDataFormat)
 	}
 
+	// TODO This test does not need anymore as we don't fix mixHash field to IstanbulDigest
 	// non zero MixDigest
-	block = makeBlockWithoutSeal(chain, engine, chain.Genesis())
-	header = block.Header()
+	/*block := makeBlockWithoutSeal(chain, engine, chain.Genesis())
+	header := block.Header()
 	header.MixDigest = common.BytesToHash([]byte("123456789"))
-	err = engine.VerifyHeader(chain, header)
+	err := engine.VerifyHeader(chain, header)
 	if err != qbftcommon.ErrInvalidMixDigest {
 		t.Errorf("error mismatch: have %v, want %v", err, qbftcommon.ErrInvalidMixDigest)
-	}
+	}*/
 
 	// invalid uncles hash
 	block = makeBlockWithoutSeal(chain, engine, chain.Genesis())
