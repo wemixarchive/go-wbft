10a361f647c15693c5d18a9298b2e734f135d103
eomti-wm <eomti@wemade.com>
2024-12-04 14:04:45 +0900

    Revert "fix: adjust time"
    This reverts commit 6a35a56b5304d31c1136e440ed02b886e323d7cc.

--- miner/simsyncer.go
+++ miner/simsyncer.go
@@ -18,16 +18,10 @@ type simSyncer struct {
 
 func (ss *simSyncer) Apply(config *qbft.Config, num *big.Int) {
 	if ss.adjustedBlockPeriod[num.Uint64()] > 0 {
-		config.Transitions = append(config.Transitions,
-			params.Transition{
-				Block:              num,
-				BlockPeriodSeconds: ss.adjustedBlockPeriod[num.Uint64()],
-			},
-			params.Transition{
-				Block:              new(big.Int).Add(num, common.Big1),
-				BlockPeriodSeconds: config.BlockPeriod,
-			},
-		)
+		config.Transitions = append(config.Transitions, params.Transition{
+			Block:              num,
+			BlockPeriodSeconds: ss.adjustedBlockPeriod[num.Uint64()],
+		})
 	}
 }
 
