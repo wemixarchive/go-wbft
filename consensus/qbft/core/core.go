caee3bd9ed8587050ad4ad8f2b1f49c52920f632
hmlee(WM) <hmlee@wemade.com>
2025-03-28 10:37:04 +0900

    fix: call NotifyNewRound only when proposer

--- consensus/qbft/core/core.go
+++ consensus/qbft/core/core.go
@@ -244,7 +244,10 @@ func (c *Core) startNewRound(round *big.Int) {
 		c.roundChangeSet.ClearLowerThan(round)
 	}
 	c.roundChangeSet.NewRound(round)
-	c.backend.NotifyNewRound(round)
+
+	if c.IsProposer() {
+		c.backend.NotifyNewRound(round)
+	}
 	// the order of NotifyNewRound() and newRoundChangeTimer() does not matter on actual consensus, but
 	// it matters on multi-engine test, so we keep the order as it is
 	c.newRoundChangeTimer()
