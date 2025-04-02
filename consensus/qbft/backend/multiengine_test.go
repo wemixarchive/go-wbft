05d1b7b731248bf244629a9254f05a7e0bbda3c1
egonspace <egon@wemade.com>
2025-04-02 13:49:55 +0900

    fix: remove useless comment

--- consensus/qbft/backend/multiengine_test.go
+++ consensus/qbft/backend/multiengine_test.go
@@ -174,7 +174,6 @@ func (env *testEnv) commitNewWork(addr common.Address) {
 	currState, _ := chain.State()
 	block, _ = engine.FinalizeAndAssemble(chain, block.Header(), currState, nil, nil, nil, nil)
 
-	// all engines try to seal
 	engine.Seal(chain, block, env.results[engine.address], env.stopSealingCh)
 }
 
