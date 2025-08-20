087126a522a9bd5236edac8b5babb5b87b7f45a4
egonspace <egon@wemade.com>
2025-08-20 13:56:31 +0900

    fix: prysm copyright

--- crypto/bls/common/interface.go
+++ crypto/bls/common/interface.go
@@ -1,19 +1,12 @@
-// Copyright 2025 The go-wemix-wbft Authors
-// This file is part of the go-wemix-wbft library.
-//
-// The go-wemix-wbft library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-wemix-wbft library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-wemix-wbft library. If not, see <http://www.gnu.org/licenses/>.
+// This file is sourced from the Prysm project, licensed under the GPLv3.
+// Original source: https://github.com/OffchainLabs/prysm/blob/develop/crypto/bls/common/interface.go
+// Copyright The Prysm Authors.
 
+// Package common provides the BLS interfaces that are implemented by the various BLS wrappers.
+//
+// This package should not be used by downstream consumers. These interfaces are re-exporter by
+// github.com/prysmaticlabs/prysm/crypto/bls. This package exists to prevent an import circular
+// dependency.
 package common
 
 // SecretKey represents a BLS secret or private key.
--- crypto/bls/signature_batch.go
+++ crypto/bls/signature_batch.go
@@ -1,18 +1,7 @@
 // Copyright 2025 The go-wemix-wbft Authors
-// This file is part of the go-wemix-wbft library.
-//
-// The go-wemix-wbft library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-wemix-wbft library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-wemix-wbft library. If not, see <http://www.gnu.org/licenses/>.
+// This file is sourced from the Prysm project, licensed under the GPLv3.
+// Original source: https://github.com/OffchainLabs/prysm/blob/develop/crypto/bls/signature_batch.go
+// Copyright The Prysm Authors.
 
 package bls
 
