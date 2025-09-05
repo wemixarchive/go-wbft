b67e868803bcdf9063a3c2aebc09ed551b2651e2
egonspace <egon@wemade.com>
2025-09-05 10:53:28 +0900

    fix: verify prepared block of round change message

--- consensus/wbft/core/roundchange.go
+++ consensus/wbft/core/roundchange.go
@@ -119,6 +119,13 @@ func (c *Core) handleRoundChangeMsg(roundChange *wbfmessage.RoundChange) error {
 		var pr *big.Int = nil
 		var pb *types.Block = nil
 		if roundChange.PreparedRound != nil && roundChange.PreparedBlock != nil && roundChange.Justification != nil && len(roundChange.Justification) > 0 {
+			// We should check the prepared block hash and sequence number
+			if roundChange.PreparedBlock.Number().Cmp(c.currentView().Sequence) != 0 {
+				return errors.New("WBFT: prepared block number does not match current sequence number in ROUND-CHANGE message")
+			}
+			if roundChange.PreparedBlock.Hash() != roundChange.PreparedDigest {
+				return errors.New("WBFT: prepared block hash does not match prepared digest in ROUND-CHANGE message")
+			}
 			prepareMessages = roundChange.Justification
 			pr = roundChange.PreparedRound
 			pb = roundChange.PreparedBlock
