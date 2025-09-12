751de914449efd80d405d3195b514eeaa6e8f661
egonspace <egon@wemade.com>
2025-09-12 09:57:21 +0900

    fix: adjust default priority fee to 100Gwei

--- miner/miner.go
+++ miner/miner.go
@@ -63,7 +63,7 @@ type Config struct {
 // DefaultConfig contains default settings for miner.
 var DefaultConfig = Config{
 	GasCeil:  105000000,
-	GasPrice: big.NewInt(params.GWei),
+	GasPrice: big.NewInt(100 * params.GWei), // 100 GWei for compatibility with Wemix 3.0
 
 	// The default recommit time is chosen as two seconds since
 	// consensus-layer usually will wait a half slot of time(6s)
