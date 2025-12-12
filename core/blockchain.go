ae0765599d82bdb1c671c8454b435ab8c48b1709
eomti-wm <eomti@wemade.com>
2025-12-12 16:06:20 +0900

    fix: enable sidechain insertion for pruned ancestors

--- core/blockchain.go
+++ core/blockchain.go
@@ -1641,17 +1641,9 @@ func (bc *BlockChain) insertChain(chain types.Blocks, setHead bool) (int, error)
 	// First block is pruned
 	case errors.Is(err, consensus.ErrPrunedAncestor):
 		if setHead {
-			if bc.Config().CroissantEnabled() {
-				log.Info("Pruned ancestor on Croissant during insertChain, discard the obsolete block", "number", block.Number(), "hash", block.Hash())
-				if bc.futureBlocks.Contains(block.Hash()) {
-					bc.futureBlocks.Remove(block.Hash())
-				}
-				return it.index, nil
-			} else {
-				// First block is pruned, insert as sidechain and reorg only if TD grows enough
-				log.Debug("Pruned ancestor, inserting as sidechain", "number", block.Number(), "hash", block.Hash())
-				return bc.insertSideChain(block, it)
-			}
+			// First block is pruned, insert as sidechain and reorg only if TD grows enough
+			log.Debug("Pruned ancestor, inserting as sidechain", "number", block.Number(), "hash", block.Hash())
+			return bc.insertSideChain(block, it)
 		} else {
 			// We're post-merge and the parent is pruned, try to recover the parent state
 			log.Debug("Pruned ancestor", "number", block.Number(), "hash", block.Hash())
