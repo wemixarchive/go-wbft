8d757e95822940375adc4f7af958d4a24c23869b
이민석(WM) <mindol8@wemade.com>
2025-12-15 08:21:10 +0900

    Feature: support eth_GetReceiptsByHash (#167)

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1016,6 +1016,10 @@ func (s *BlockChainAPI) GetBlockReceipts(ctx context.Context, blockNrOrHash rpc.
 	return result, nil
 }
 
+func (s *BlockChainAPI) GetReceiptsByHash(ctx context.Context, blockHash common.Hash) ([]map[string]interface{}, error) {
+	return s.GetBlockReceipts(ctx, rpc.BlockNumberOrHashWithHash(blockHash, true))
+}
+
 // OverrideAccount indicates the overriding fields of account during the execution
 // of a message call.
 // Note, state and stateDiff can't be specified at the same time. If state is
