stable-net/go-stablenet

feat: EIP-7702 Set Code Transaction support (#42)
* chore: update Go version to 1.21

* feat: implement EIP-7702 set code transaction type

Add support for EIP-7702 (SetCodeTx) which allows EOAs to temporarily
delegate their code execution to another account.

Changes include:
- Add SetCodeTx (type 0x04) transaction type and Authorization struct
- Add anzeonSigner for EIP-7702 transaction signing
- Implement delegation designator resolution in EVM
- Add EIP-7702 specific gas calculation for CALL variants
- Update EXTCODE* opcodes to handle delegation designators
- Add prestate tracer support for authorization list
- Update t8n tool and tests for SetCodeTx

Based on https://github.com/ethereum/go-ethereum/pull/30078

* fix: complete EIP-7702 authorization marshaling and eth_call support

Based on https://github.com/ethereum/go-ethereum/pull/30926

* refactor: improve EIP-7702 API naming

- Rename Authorization to SetCodeAuthorization
- Rename SignAuth to SignSetCode with key-first parameter order

Based on https://github.com/ethereum/go-ethereum/pull/30933

* fix: rename SetCodeAuthorization 'v' to 'yParity'

The API spec requires the name yParity.

Based on:
- https://github.com/ethereum/go-ethereum/commit/73a4ecf675f6714e1fdf49904ce9426841de2e02
- https://github.com/ethereum/go-ethereum/pull/30936

* refactor: rename AuthList to SetCodeAuthorizations

Use SetCode prefix consistently in internal APIs for the authorization
list, as a follow-up to SetCodeAuthorization type renaming.

Based on https://github.com/ethereum/go-ethereum/pull/30935

* refactor: change SetCodeTx.ChainID to uint256

Change ChainID field type from uint64 to uint256.Int in SetCodeTx and
SetCodeAuthorization for consistency with other chain ID handling.

Also removes unused json/gencodec tags from signature value fields
in DynamicFeeTx, BlobTx, SetCodeTx, and FeeDelegateDynamicFeeTx.

Based on https://github.com/ethereum/go-ethereum/pull/30982

* fix: correct chainId check for anzeonSigner

Use 

cmd/evm/t8n_test.go
@@ -284,6 +284,14 @@ func TestT8n(t *testing.T) {
 			output: t8nOutput{alloc: true, result: true},
 			expOut: "exp.json",
 		},
+		{ // Anzeon test, EIP-7702 transaction
+			base: "./testdata/33",
+			input: t8nInput{
+				"alloc.json", "txs.json", "env.json", "Anzeon", "",
+			},
+			output: t8nOutput{alloc: true, result: true},
+			expOut: "exp.json",
+		},
 	} {
 		args := []string{"t8n"}
 		args = append(args, tc.output.get()...)

wemixarchive/go-wbft

cmd/evm/t8n_test.go
@@ -284,6 +284,14 @@ func TestT8n(t *testing.T) {
 			output: t8nOutput{alloc: true, result: true},
 			expOut: "exp.json",
 		},
+		{ // Croissant test, EIP-7702 transaction
+			base: "./testdata/33",
+			input: t8nInput{
+				"alloc.json", "txs.json", "env.json", "Croissant", "",
+			},
+			output: t8nOutput{alloc: true, result: true},
+			expOut: "exp.json",
+		},
 	} {
 		args := []string{"t8n"}
 		args = append(args, tc.output.get()...)